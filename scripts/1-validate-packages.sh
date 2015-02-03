0d4ecb6359bcb2a9e5fb85d1fd58d6b101d6b184
Connor Doyle <connor@mesosphere.io>
2015-02-03 11:44:19 -0800

    Update shabangs to bash to match used opts.

--- scripts/1-validate-packages.sh
+++ scripts/1-validate-packages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -o errexit -o nounset -o pipefail
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
--- scripts/3-validate-index.sh
+++ scripts/3-validate-index.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -o errexit -o nounset -o pipefail
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 UNIVERSE_DIR="$SCRIPTS_DIR/..";
 SCHEMA_DIR=$UNIVERSE_DIR/repo/meta/schema
 
-echo "\nValidating index...";
+echo "Validating index...";
 jsonschema -i $UNIVERSE_DIR/repo/meta/index.json $SCHEMA_DIR/index-schema.json;
 
 echo "OK";
