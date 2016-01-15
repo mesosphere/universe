15e26fe289f4bddd8ba61b22c31c2d007d17c7b1
José Armando García Sancio <jsancio@gmail.com>
2016-01-15 10:43:13 -0800

    Implements scripts for downloading all assets
    This script is able to download all of the HTTP resources and Docker
    images. The temp directory used can be overwritten by using the TMPDIR
    environment variable.
    
    This script only works with version-2.x of Universe.
    
    To create a local universe zip file, using the current directory as the
    temporary directory, run the following command:
    
    ```
    > TMPDIR=. python3 scripts/local-universe.py --repository repo/packages/ --out-file
    local_universe.zip --sudo
    ```
    
    This creates a file called `local_universe.zip` which contains the
    following information:
    
    ```
    > unzip -l local_universe.zip
    Archive:  local_universe.zip
      Length      Date    Time    Name
    ---------  ---------- -----   ----
         4148  01-15-2016 10:36   arangodb/images/arangodb_small.png
        10128  01-15-2016 10:36   arangodb/images/arangodb_medium.png
        47394  01-15-2016 10:36   arangodb/images/arangodb_large.png
         1920  01-15-2016 10:36   cassandra/images/cassandra-small.png
         5070  01-15-2016 10:36   cassandra/images/cassandra-medium.png
        17969  01-15-2016 10:36   cassandra/images/cassandra-large.png
     47058747  01-15-2016 10:36   cassandra/uris/jre-7u76-linux-x64.tar.gz
     70886405  01-15-2016 10:36   cassandra/uris/cassandra-mesos-0.2.0-1.tar.gz
         3038  01-15-2016 10:36   chronos/images/icon-service-chronos-small.png
        10074  01-15-2016 10:36   chronos/images/icon-service-chronos-medium.png
        23663  01-15-2016 10:36   chronos/images/icon-service-chronos-large.png
         2323  01-15-2016 10:36   hdfs/images/icon-service-hdfs-small.png
         6396  01-15-2016 10:36   hdfs/images/icon-service-hdfs-medium.png
        15829  01-15-2016 10:36   hdfs/images/icon-service-hdfs-large.png
     47058747  01-15-2016 10:36   hdfs/uris/jre-7u76-linux-x64.tar.gz
    117885110  01-15-2016 10:36   hdfs/uris/hdfs-mesos-0.1.5.tgz
         4306  01-15-2016 10:36   kafka/images/icon-small.png
         2748  01-15-2016 10:36   kafka/images/icon-medium.png
         7847  01-15-2016 10:36   kafka/images/icon-large.png
     15998492  01-15-2016 10:37   kafka/uris/kafka-mesos-0.9.2.0.jar
     16162559  01-15-2016 10:37   kafka/uris/kafka_2.10-0.8.2.1.tgz
     48993499  01-15-2016 10:37   kafka/uris/jre-7u79-openjdk.tgz
         1839  01-15-2016 10:37   marathon/images/icon-service-marathon-small.png
         3759  01-15-2016 10:37   marathon/images/icon-service-marathon-medium.png
        10927  01-15-2016 10:37   marathon/images/icon-service-marathon-large.png
         1501  01-15-2016 10:37   spark/images/icon-service-spark-small.png
         3040  01-15-2016 10:37   spark/images/icon-service-spark-medium.png
         9028  01-15-2016 10:37   spark/images/icon-service-spark-large.png
          621  01-15-2016 10:37   spark/uris/log4j.properties
    2745697792  01-15-2016 10:38   docker-images.tar
    ---------                     -------
    3109934919                     30 files
    ```

--- scripts/json_dup_key_check.py
+++ scripts/json_dup_key_check.py
@@ -4,24 +4,28 @@ import os
 import sys
 import json
 
+
 class DuplicatedKeysException(Exception):
-  pass
+    pass
+
 
 def json_checker(pair):
-  ret = {} 
-  for key, value in pair:
-    if key in ret:
-      raise DuplicatedKeysException("Duplicate key %r in json document" % key)
-    else:
-      ret[key]=value
-  return ret
+    ret = {}
+    for key, value in pair:
+        if key in ret:
+            raise DuplicatedKeysException(
+                "Duplicate key {!r} in json document".format(key))
+        else:
+            ret[key] = value
+    return ret
 
 if len(sys.argv) != 2:
-  sys.stderr.write("Syntax: %s path/to/file.json\n" % os.path.basename(__file__))
-  sys.exit(1)
+    sys.stderr.write(
+        "Syntax: {} path/to/file.json\n".format(os.path.basename(__file__)))
+    sys.exit(1)
 
 try:
-  json.load(open(sys.argv[1]), object_pairs_hook=json_checker)
+    json.load(open(sys.argv[1]), object_pairs_hook=json_checker)
 except DuplicatedKeysException as e:
-  sys.stderr.write("Error validating %s: %s\n" % (sys.argv[1], e.args[0]))
-  sys.exit(1)
+    sys.stderr.write("Error validating %s: %s\n" % (sys.argv[1], e.args[0]))
+    sys.exit(1)
