c2b897b2d645aa9d79207d751fef666886525a72
BenWhitehead <ben.whitehead@mesosphere.io>
2016-05-26 18:04:27 -0700

    Add docker config to build "universe server"
    Introduce Dockerfile and build script necessary to build a Universe Server which can be ran in development environments or on a DC/OS cluster via marathon.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,13 @@
 set -o errexit -o nounset -o pipefail
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+REPO_BASE_DIR=${SCRIPTS_DIR}/..
 
 echo "Building the universe!";
 
+mkdir -p ${REPO_BASE_DIR}/target/;
+
 $SCRIPTS_DIR/"0-validate-version.sh";
 $SCRIPTS_DIR/"1-validate-packages.sh";
+$SCRIPTS_DIR/gen-universe.py --repository=${REPO_BASE_DIR}/repo/packages/ --out-dir=${REPO_BASE_DIR}/target/;
 
