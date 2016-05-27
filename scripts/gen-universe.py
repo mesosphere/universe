932f65cda7c3960bc582f97c9dfe7bf0da99ef83
José Armando García Sancio <jsancio@gmail.com>
2016-05-27 15:02:19 -0700

    Initial commit for generating verzion-2 zip

--- scripts/gen-universe.py
+++ scripts/gen-universe.py
@@ -4,7 +4,10 @@ import argparse
 import base64
 import json
 import pathlib
+import shutil
 import sys
+import tempfile
+import zipfile
 
 
 def main():
@@ -50,6 +53,14 @@ def main():
 
     with universe_file_path.open('w') as universe_file:
         json.dump(universe, universe_file)
+
+    universe_old_zip = args.outdir / 'universe-1.6.1.zip'
+
+    with tempfile.NamedTemporaryFile() as temp_file:
+        with zipfile.ZipFile(temp_file, mode='w') as zip_file:
+            render_universe_zip(zip_file, universe['packages'])
+
+        shutil.copy(temp_file.name, str(universe_old_zip))
 
 
 def package_path(root, package_name, release_version):
@@ -222,6 +233,103 @@ def enumerate_dcos_packages(packages_path):
         for package_path in letter_path.iterdir():
             for release_version in package_path.iterdir():
                 yield (package_path.name, int(release_version.name))
+
+
+def render_universe_zip(zip_file, packages):
+    """Populates a zipfile from a list of universe v3 packages. This function
+    creates directories to be backwards compatible with legacy Cosmos.
+
+    :param zip_file: zipfile where we need to write the packages
+    :type zip_file: zipfile.ZipFile
+    :param packages: list of packages
+    :type packages: [dict]
+    :rtype: None
+    """
+
+    # TODO: What do we need to write?
+    # write universe/repo/meta/index.json?
+    # write universe/repo/meta/version.json?
+
+    currentDir = pathlib.Path('universe')
+
+    create_dir_in_zip(zip_file, currentDir)
+
+    currentDir = currentDir / 'repo'
+    create_dir_in_zip(zip_file, currentDir)
+
+    currentDir = currentDir / 'packages'
+    create_dir_in_zip(zip_file, currentDir)
+
+    packages = sorted(
+        packages,
+        key=lambda package: (package['name'], package['releaseVersion']))
+
+    currentLetter = ''
+    currentPackageName = ''
+    for package in packages:
+        if currentLetter != package['name'][:1].upper():
+            currentLetter = package['name'][:1].upper()
+            create_dir_in_zip(zip_file, currentDir / currentLetter)
+
+        if currentPackageName != package['name']:
+            currentPackageName = package['name']
+            create_dir_in_zip(
+                zip_file,
+                currentDir / currentLetter / currentPackageName)
+
+        package_directory = (
+            currentDir /
+            currentLetter /
+            currentPackageName /
+            str(package['releaseVersion'])
+        )
+        create_dir_in_zip(zip_file, package_directory)
+
+        write_package_in_zip(zip_file, package_directory, package)
+
+
+def create_dir_in_zip(zip_file, directory):
+    zip_file.writestr(str(directory) + '/', b'')
+
+
+def write_package_in_zip(zip_file, path, package):
+    package = package.copy()
+    package.pop('releaseVersion')
+
+    resource = package.pop('resource', None)
+    if resource:
+        zip_file.writestr(
+            str(path / 'resource.json'),
+            json.dumps(resource))
+
+    marathon_template = package.pop(
+        'marathon',
+        {}
+    ).get(
+        'v2AppMustacheTemplate'
+    )
+    if marathon_template:
+        zip_file.writestr(
+            str(path / 'marathon.json.mustache'),
+            base64.standard_b64decode(marathon_template))
+
+    config = package.pop('config', None)
+    if config:
+        zip_file.writestr(
+            str(path / 'config.json'),
+            json.dumps(config))
+
+    command = package.pop('pip', None)
+    if command:
+        zip_file.writestr(
+            str(path / 'command.json'),
+            json.dumps({'pip': command}))
+
+    # TODO: Make sure that we don't have any extra fields like:
+    # minDcosReleaseVersion
+    zip_file.writestr(
+        str(path / 'package.json'),
+        json.dumps(package))
 
 
 if __name__ == '__main__':
