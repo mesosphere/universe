dfcb736fabd3d27e092f513f92915270c9d8d76a
José Armando García Sancio <jsancio@users.noreply.github.com>
2016-06-01 10:53:58 -0700

    Gen universe (#471)
    This comment includes the following changes
    
    * Generate verzion-2 zip for 1.6.1 and 1.7
    * Add support for rendering json for 1.8 releases

--- scripts/gen-universe.py
+++ scripts/gen-universe.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+from distutils.version import LooseVersion
 import argparse
 import base64
+import itertools
 import json
 import pathlib
+import shutil
 import sys
+import tempfile
+import zipfile
 
 
 def main():
@@ -35,21 +40,99 @@ def main():
             args.repository))
         return
 
-    universe = {
-        'packages': [
-            generate_package_from_path(
-                args.repository,
-                package_name,
-                release_version)
-            for package_name, release_version
-            in enumerate_dcos_packages(args.repository)
-        ]
-    }
+    packages = [
+        generate_package_from_path(
+            args.repository,
+            package_name,
+            release_version)
+        for package_name, release_version
+        in enumerate_dcos_packages(args.repository)
+    ]
 
-    universe_file_path = args.outdir / 'universe.json'
+    # Render entire universe
+    with (args.outdir / 'universe.json').open('w') as universe_file:
+        json.dump({'packages': packages}, universe_file)
 
-    with universe_file_path.open('w') as universe_file:
-        json.dump(universe, universe_file)
+    # Render empty json
+    with (args.outdir / 'repo-empty-v3.json').open('w') as universe_file:
+        json.dump({'packages': []}, universe_file)
+
+    # Render 1.8 json
+    with (args.outdir / 'repo-up-to-1.8.json').open('w') as universe_file:
+        json.dump(
+            {'packages': list(filter(filter_1_8, packages))},
+            universe_file)
+
+    # Render zip universe for 1.6.1
+    with tempfile.NamedTemporaryFile() as temp_file:
+        with zipfile.ZipFile(temp_file, mode='w') as zip_file:
+            render_universe_zip(
+                zip_file,
+                filter(filter_1_6_1, packages)
+            )
+
+        shutil.copy(temp_file.name, str(args.outdir / 'repo-up-to-1.6.1.zip'))
+
+    # Render zip universe for 1.7
+    with tempfile.NamedTemporaryFile() as temp_file:
+        with zipfile.ZipFile(temp_file, mode='w') as zip_file:
+            render_universe_zip(
+                zip_file,
+                filter(filter_1_7, packages)
+            )
+
+        shutil.copy(temp_file.name, str(args.outdir / 'repo-up-to-1.7.zip'))
+
+
+def filter_1_6_1(package):
+    """Predicate for checking for 1.6.1 or less packages
+
+    :param package: package dictionary
+    :type package: dict
+    :rtype: bool
+    """
+
+    package_version = LooseVersion(
+        package.get('minDcosReleaseVersion', '0.0')
+    )
+
+    filter_version = LooseVersion("1.6.1")
+
+    return package_version <= filter_version
+
+
+def filter_1_7(package):
+    """Predicate for checking for 1.7 or less packages
+
+    :param package: package dictionary
+    :type package: dict
+    :rtype: bool
+    """
+
+    package_version = LooseVersion(
+        package.get('minDcosReleaseVersion', '0.0')
+    )
+
+    filter_version = LooseVersion("1.7")
+
+    return package_version <= filter_version
+
+
+def filter_1_8(package):
+    """Predicate for checking for 1.8 or less packages
+
+    :param package: package dictionary
+    :type package: dict
+    :rtype: bool
+    """
+
+    package_version = LooseVersion(
+        package.get('minDcosReleaseVersion', '0.0')
+    )
+
+    filter_version = LooseVersion("1.8")
+
+    return package_version <= filter_version
 
 
 def package_path(root, package_name, release_version):
@@ -222,6 +305,171 @@ def enumerate_dcos_packages(packages_path):
         for package_path in letter_path.iterdir():
             for release_version in package_path.iterdir():
                 yield (package_path.name, int(release_version.name))
+
+
+def render_universe_zip(zip_file, packages):
+    """Populates a zipfile from a list of universe v3 packages. This function
+    creates directories to be backwards compatible with legacy Cosmos.
+
+    :param zip_file: zipfile where we need to write the packages
+    :type zip_file: zipfile.ZipFile
+    :param packages: list of packages
+    :type packages: [dict]
+    :rtype: None
+    """
+
+    packages = sorted(
+        packages,
+        key=lambda package: (package['name'], package['releaseVersion']))
+
+    root = pathlib.Path('universe')
+
+    create_dir_in_zip(zip_file, root)
+
+    create_dir_in_zip(zip_file, root / 'repo')
+
+    create_dir_in_zip(zip_file, root / 'repo' / 'meta')
+    zip_file.writestr(
+        str(root / 'repo' / 'meta' / 'index.json'),
+        json.dumps(create_index(packages)))
+
+    packagesDir = root / 'repo' / 'packages'
+    create_dir_in_zip(zip_file, packagesDir)
+
+    currentLetter = ''
+    currentPackageName = ''
+    for package in packages:
+        if currentLetter != package['name'][:1].upper():
+            currentLetter = package['name'][:1].upper()
+            create_dir_in_zip(zip_file, packagesDir / currentLetter)
+
+        if currentPackageName != package['name']:
+            currentPackageName = package['name']
+            create_dir_in_zip(
+                zip_file,
+                packagesDir / currentLetter / currentPackageName)
+
+        package_directory = (
+            packagesDir /
+            currentLetter /
+            currentPackageName /
+            str(package['releaseVersion'])
+        )
+        create_dir_in_zip(zip_file, package_directory)
+
+        write_package_in_zip(zip_file, package_directory, package)
+
+
+def create_dir_in_zip(zip_file, directory):
+    """Create a directory in a zip file
+
+    :param zip_file: zip file where the directory will get created
+    :type zip_file: zipfile.ZipFile
+    :param directory: path for the directory
+    :type directory: pathlib.Path
+    :rtype: None
+    """
+
+    zip_file.writestr(str(directory) + '/', b'')
+
+
+def write_package_in_zip(zip_file, path, package):
+    """Write packages files in the zip file
+
+    :param zip_file: zip file where the files will get created
+    :type zip_file: zipfile.ZipFile
+    :param path: path for the package directory. E.g.
+                 universe/repo/packages/M/marathon/0
+    :type path: pathlib.Path
+    :param package: package information dictionary
+    :type package: dict
+    :rtype: None
+    """
+
+    package = package.copy()
+    package.pop('releaseVersion')
+
+    resource = package.pop('resource', None)
+    if resource:
+        zip_file.writestr(
+            str(path / 'resource.json'),
+            json.dumps(resource))
+
+    marathon_template = package.pop(
+        'marathon',
+        {}
+    ).get(
+        'v2AppMustacheTemplate'
+    )
+    if marathon_template:
+        zip_file.writestr(
+            str(path / 'marathon.json.mustache'),
+            base64.standard_b64decode(marathon_template))
+
+    config = package.pop('config', None)
+    if config:
+        zip_file.writestr(
+            str(path / 'config.json'),
+            json.dumps(config))
+
+    command = package.pop('pip', None)
+    if command:
+        zip_file.writestr(
+            str(path / 'command.json'),
+            json.dumps({'pip': command}))
+
+    # TODO: Make sure that we don't have any extra fields like:
+    # minDcosReleaseVersion
+    zip_file.writestr(
+        str(path / 'package.json'),
+        json.dumps(package))
+
+
+def create_index(packages):
+    """Create an index for all of the packages
+
+    :param packages: list of packages
+    :type packages: [dict]
+    :rtype: dict
+    """
+
+    index = {
+        'version': "2.0.0-rc2",
+        'packages': [
+            create_index_entry(same_packages)
+            for _, same_packages
+            in itertools.groupby(packages, key=lambda package: package['name'])
+        ]
+    }
+
+    return index
+
+
+def create_index_entry(packages):
+    """Create index entry from packages with the same name.
+
+    :param packages: list of packages with the same name
+    :type packages: [dict]
+    :rtype: dict
+    """
+
+    entry = {
+        'versions': {}
+    }
+
+    for package in packages:
+        entry.update({
+            'name': package['name'],
+            'currentVersion': package['version'],
+            'description': package['description'],
+            'framework': package.get('framework', False),
+            'tags': package['tags'],
+            'selected': package.get('selected', False)
+        })
+
+        entry['versions'][package['version']] = str(package['releaseVersion'])
+
+    return entry
 
 
 if __name__ == '__main__':
