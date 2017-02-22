e27008714e10c093bef00389ae133d6542d5abe3
Jesus E. Larios Murillo <jlariosmurillo@mesosphere.io>
2017-02-21 16:11:31 -0800

    Rename variable

--- scripts/validate-packages.py
+++ scripts/validate-packages.py
@@ -58,8 +58,8 @@ def _validate_revision(given_package, revision, path):
     packaging_version = package_json.get("packagingVersion", "2.0")
 
     # validate upgrades version
-    min_dcos_version = package_json.get("minDcosReleaseVersion")
-    if packaging_version == "4.0" and LooseVersion(min_dcos_version) < LooseVersion("1.10"):
+    min_dcos_release_version = package_json.get("minDcosReleaseVersion")
+    if packaging_version == "4.0" and LooseVersion(min_dcos_release_version) < LooseVersion("1.10"):
         sys.exit("\tERROR\n\nminDcosReleaseVersion must be greater than or equal to 1.10")
 
     # validate command.json
