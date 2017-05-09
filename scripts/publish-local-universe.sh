e7e062e1208215e94cc863fc8c8e5c023b3f76f1
José Armando García Sancio <jsancio@gmail.com>
2017-05-09 16:26:09 -0700

    Specify the region

--- scripts/publish-local-universe.sh
+++ scripts/publish-local-universe.sh
@@ -7,4 +7,4 @@ REPO_BASE_DIR=${SCRIPTS_DIR}/..
 S3_DEPLOY_BUCKET="s3://downloads.mesosphere.com/universe/public/"
 
 echo "Uploading local universe to: ${S3_DEPLOY_BUCKET}"
-aws s3 cp "${REPO_BASE_DIR}"/docker/local-universe/local-universe.tar.gz "${S3_DEPLOY_BUCKET}"
+aws s3 cp --region us-east-1 "${REPO_BASE_DIR}"/docker/local-universe/local-universe.tar.gz "${S3_DEPLOY_BUCKET}"
