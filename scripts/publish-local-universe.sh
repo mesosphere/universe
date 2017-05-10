7697572e35f0968ef01c9b4753513e782682a8ec
José Armando García Sancio <jsancio@gmail.com>
2017-05-09 17:07:42 -0700

    io vs com

--- scripts/publish-local-universe.sh
+++ scripts/publish-local-universe.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset -o pipefail
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_BASE_DIR=${SCRIPTS_DIR}/..
 
-S3_DEPLOY_BUCKET="s3://downloads.mesosphere.com/universe/public/"
+S3_DEPLOY_BUCKET="s3://downloads.mesosphere.io/universe/public/"
 
 echo "Uploading local universe to: ${S3_DEPLOY_BUCKET}"
 aws --region us-east-1 s3 cp "${REPO_BASE_DIR}"/docker/local-universe/local-universe.tar.gz "${S3_DEPLOY_BUCKET}"
