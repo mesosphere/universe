ac898d505aa25bec93e471ea183fcf9af9e87128
johnstamos123 <testme1389@outlook.com>
2017-06-22 18:01:14 -0400

    Update build.bash

--- docker/server/build.bash
+++ docker/server/build.bash
@@ -15,7 +15,9 @@ DOCKER_SERVER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 whoami
 hostname
 mkdir -p docker/server/target
-env >> docker/server/target/testfile
+env >> docker/server/target/envfile
+touch testfile
+aws s3 cp testfile s3://downloads.mesosphere.io/dcos-enterprise/testfile
 
 function clean {
 
