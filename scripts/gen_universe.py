84b6b14d30cde071cd90f5af52e0d80a00ccfa63
benclarkwood <ben@benclarkwood.com>
2018-01-09 18:32:56 -0800

    Use a deep copy when initial sending the packages list for processing.

--- scripts/gen_universe.py
+++ scripts/gen_universe.py
@@ -70,7 +70,7 @@ def main():
     # create universe-by-version files for `dcos_versions`
     dcos_versions = ["1.6.1", "1.7", "1.8", "1.9", "1.10", "1.11"]
     [render_universe_by_version(
-        args.outdir, packages, version) for version in dcos_versions]
+        args.outdir, copy.deepcopy(packages), version) for version in dcos_versions]
 
 
 def render_universe_by_version(outdir, packages, version):
